8c568a25f17d98d41fd1f32d739f0ff13254bab0
Perin <joaovperin@live.com>
2018-02-26 17:30:46 -0300

    Database connection

--- WebService/FuelControl/src/main/java/br/com/jpe/fuelcontrol/services/Auth.java
+++ WebService/FuelControl/src/main/java/br/com/jpe/fuelcontrol/services/Auth.java
@@ -5,6 +5,10 @@
  */
 package br.com.jpe.fuelcontrol.services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Service;
@@ -19,6 +23,12 @@ public class Auth {
 
     /** Users cache (for testing purposes only) */
     private static final Map<String, String> USERS;
+    /** Factory for connections */
+    private final ConnFactory connFactory;
+
+    public Auth(ConnFactory factory) {
+        this.connFactory = factory;
+    }
 
     static {
         USERS = new HashMap<>();
@@ -35,9 +45,19 @@ public class Auth {
      * @return boolean
      */
     public boolean login(String user, String pass) {
-        String userPassword = USERS.get(user.toLowerCase());
-        if (userPassword != null) {
-            return userPassword.equals(pass);
+        try (Connection conn = connFactory.getConn()) {
+            String sql = "SELECT User, Pass FROM Users WHERE User = ?";
+            try (PreparedStatement st = conn.prepareStatement(sql)) {
+                st.setString(1, user);
+                st.setString(2, pass);
+                try (ResultSet rs = st.executeQuery()) {
+                    if (rs.next()) {
+                        String userPassword = rs.getString(2);
+                        return userPassword.equals(pass);
+                    }
+                }
+            }
+        } catch (SQLException ex) {
         }
         return false;
     }
